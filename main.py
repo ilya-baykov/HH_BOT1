c81e1408a4fa4927901b3a47108bd17c1d4a7853
ilyab <i-baykov@internet.ru>
2024-12-03 10:31:55 +0300

    Реализовал запросы к ЯндексГПТ

---
+++ main.py
@@ -0,0 +1,31 @@
+from configuration_file_handler.file_dataclass import RecruiterDataParams
+from configuration_file_handler.read_file import RecruiterDataReader
+from neural_gateway.neural_gateway_client import ChatGPT, YaGPT
+from neural_gateway.prompt_generator import PromptGenerator
+from pdf_parser.pdf_reader import PDFTextExtractor
+
+from logger import logger
+
+
+def main() -> None:
+    pdf_path = r"C:\Users\ilyab\PycharmProjects\HH_BOT_1\resumes\Менеджер по продажам_3.pdf"
+    resume_text = PDFTextExtractor.get_text(pdf_path=pdf_path)
+
+    data_params_file_path = r"C:\Users\ilyab\PycharmProjects\HH_BOT_1\configuration_files\Менеджер дистанционных продаж.xlsx"
+    recruiter_params: RecruiterDataParams = RecruiterDataReader(file_path=data_params_file_path).read_data()
+
+    prompt = PromptGenerator.generate_prompt(recruiter_params=recruiter_params, resume_text=resume_text)
+
+    model_gpt_4o_mini = ChatGPT()
+    result_1 = model_gpt_4o_mini.get_answer(prompt=prompt)
+    logger.debug(f"Результат для модели model_gpt_4o_mini: ")
+    logger.info(result_1)
+
+    model_ya_gpt = YaGPT()
+    result_2 = model_ya_gpt.get_answer(prompt=prompt)
+    logger.debug(f"Результат для модели YaGPT: ")
+    logger.info(result_2)
+
+
+if __name__ == '__main__':
+    main()
